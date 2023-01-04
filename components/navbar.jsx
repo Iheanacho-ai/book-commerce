b675ef6d58450bbe2230cd19ff1c46efe88c2e7c
Amarachi Iheanacho <amarachi2812@gmail.com>
2023-01-04 19:18:31 +0100

    first draft of the stripe subscription

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -36,13 +36,13 @@ function Navbar() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
                         </svg>
                     </li>
-                    <li><Link className="text-sm text-gray-400 hover:text-gray-500" href="/pricing">Subscription</Link></li>
+                    <li><Link className="text-sm text-gray-400 hover:text-gray-500" href="/pricing">Pricing</Link></li>
                     <li className="text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
                         </svg>
                     </li>
-                    <li><Link className="text-sm text-gray-400 hover:text-gray-500" href="#">Pricing</Link></li>
+                    <li><Link className="text-sm text-gray-400 hover:text-gray-500" href="#">Stuff</Link></li>
                     <li className="text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
@@ -79,10 +79,10 @@ function Navbar() {
                                         <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/catalog-page">Catalog</Link>
                                     </li>
                                     <li className="mb-1">
-                                        <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/pricing">Subscriptions</Link>
+                                        <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/pricing">Pricing</Link>
                                     </li>
                                     <li className="mb-1">
-                                        <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Pricing</Link>
+                                        <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Stuff</Link>
                                     </li>
                                     <li className="mb-1">
                                         <Link className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Contact</Link>
--- components/product-display.jsx
+++ components/product-display.jsx
@@ -1,6 +1,24 @@
 import {useState} from 'react';
 
-const ProductDisplay = () => {
+const subscriptionPackages = [
+    {
+        subscriptionPackage: "Starter",
+        price: "30",
+        priceId: "price_1MLWJFBpQhjuYMrzoyyf6uwq"
+    },
+    {
+        subscriptionPackage: "Pro",
+        price: "50",
+        priceId: "price_1MLWKoBpQhjuYMrzcjP4I4xm"
+    },
+    {
+        subscriptionPackage: "Family",
+        price: "100",
+        priceId: "price_1MLWXPBpQhjuYMrzCRJnTnZF"
+    },
+]
+
+const ProductDisplay = () => {    
     const [priceId, setPriceId] = useState('');
     const [customerId, setCustomerId] = useState('cus_N6Z4u3wocqYsS0');
 
@@ -39,30 +57,35 @@ const ProductDisplay = () => {
                 </div>
 
                 <div className="mt-[20px] grid grid-cols-3 gap-[20px]">
-                    <div key="1" className="w-full bg-[#fff] rounded-[10px] shadow-[0px 1px 2px #E1E3E5] border border-[#E1E3E5] divide-y">
-                        <div className="pt-[15px] px-[25px] pb-[25px]">
-                            <div className="flex justify-end">
-                                <div className="bg-[#F6F6F7] rounded-[20px] flex justify-center align-center px-[12px]">
-                                    <p className="text-[#00153B] text-[12px] leading-[28px] font-bold">
-                                        Starter
-                                    </p>
+                    {
+                        subscriptionPackages.map(({subscriptionPackage, price, priceId}) => (
+                            <div key="1" className="w-full bg-[#fff] rounded-[10px] shadow-[0px 1px 2px #E1E3E5] border border-[#E1E3E5] divide-y">
+                                <div className="pt-[15px] px-[25px] pb-[25px]">
+                                    <div className="flex justify-end">
+                                        <div className="bg-[#F6F6F7] rounded-[20px] flex justify-center align-center px-[12px]">
+                                            <p className="text-[#00153B] text-[12px] leading-[28px] font-bold">
+                                                {subscriptionPackage}
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex flex-col justify-center px-2 py-8 space-y-4">
+                                        <p className="text-5xl font-bold">{price}$
+                                            <span className="text-xl"> /mo</span>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="pt-[25px] px-[25px] pb-[35px]">
+                                        <div className="mt-[25px]">
+                                            <input type="hidden" name="lookup_key" />
+                                            <button className="bg-[#006EF5] rounded-[5px] py-[15px] px-[25px] text-[#fff] text-[14px] leading-[17px] font-semibold"  id={priceId} type="button" onClick={createSubscription}>Choose Plan</button>
+                                        </div>
                                 </div>
                             </div>
 
-                            <div className="flex flex-col justify-center px-2 py-8 space-y-4">
-                                <p className="text-5xl font-bold">30$
-                                    <span className="text-xl"> /mo</span>
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="pt-[25px] px-[25px] pb-[35px]">
-                                <div className="mt-[25px]">
-                                    <input type="hidden" name="lookup_key" />
-                                    <button className="bg-[#006EF5] rounded-[5px] py-[15px] px-[25px] text-[#fff] text-[14px] leading-[17px] font-semibold"  id="price_1MLWJFBpQhjuYMrzoyyf6uwq" type="button" onClick={createSubscription}>Choose Plan</button>
-                                </div>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
