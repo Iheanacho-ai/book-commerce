16915f52dbf8e25cc512435c93cc3037fc1026a4
Amarachi Iheanacho <amarachi2812@gmail.com>
2023-02-26 23:49:30 +0100

    changed the redirect link

--- utils/index.ts
+++ utils/index.ts
@@ -13,4 +13,4 @@ const getStripe = () => {
 const getstripe = getStripe()
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
