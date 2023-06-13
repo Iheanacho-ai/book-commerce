d4cbe01e6cfec709681f1115db2f0876bdfe789b
Amarachi Iheanacho <amarachi2812@gmail.com>
2023-06-13 23:38:43 +0100

    added cron job

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,10 +5,13 @@ import { useState } from 'react'
 // import type { AppProps } from 'next/app'
 import NavBar from '../components/navbar'
 import Footer from '../components/footer'
+import { Cron } from 'react-js-cron'
+import 'react-js-cron/dist/styles.css'
 
 export default function App({ Component, pageProps }) {
   // Create a new supabase browser client on every first render.
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  const [value, setValue] = useState('0 20 28 * 6')
   return (
     <SessionContextProvider
       supabaseClient={supabaseClient}
@@ -17,6 +20,7 @@ export default function App({ Component, pageProps }) {
       <div>
         <NavBar/>
         <div className= 'bookify-body'>
+          <Cron value={value} setValue={setValue} />
           <Component {...pageProps} />
         </div>
         <Footer/>
